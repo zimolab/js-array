cd8772d8242ed8f685baf2f27e4faa188d37f12a
xqs1995 <zyzz19951230@163.com>
2021-08-16 16:11:26 +0800

    feat(Core-API): add new api: getAny()

--- src/main/kotlin/com/github/zimolab/jsarray/base/JsArrayInterface.kt
+++ src/main/kotlin/com/github/zimolab/jsarray/base/JsArrayInterface.kt
@@ -27,6 +27,7 @@ interface JsArrayInterface<T> {
 
     operator fun set(index: Int, value: T?)
     operator fun get(index: Int): T?
+    fun getAny(index: Int): Any?
     fun concat(other: JsArrayInterface<T>): JsArrayInterface<T>
     fun join(separator: String = ","): String
     fun reverse(): JsArrayInterface<T>
